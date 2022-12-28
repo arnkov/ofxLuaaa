efd0f174e45af9936af048bbe719e299247dec0d
Arne Koenig <koenigarne@web.de>
2022-12-28 19:56:33 +0100

    some small fixes

--- src/ofxLuaaa.cpp
+++ src/ofxLuaaa.cpp
@@ -5,11 +5,15 @@ ofxLuaaa::ofxLuaaa(){}
 ofxLuaaa::~ofxLuaaa() {}
 
 
-void ofxLuaaa::runScript(lua_State* ls, const std::string& script) {
+void ofxLuaaa::runScript(lua_State* ls, std::string& script) {
 	if (ls != NULL) {
+		clearLua(ls);
+		//std::string text = ofBufferFromFile(script);
+		//luaL_dostring(ls, text.c_str());
 		std::string scriptPath = ofFilePath::getAbsolutePath(ofToDataPath(script));
 		int luaFile = luaL_loadfile(ls, scriptPath.c_str());
 		luaFile = lua_pcall(ls, 0, LUA_MULTRET, 0);
+		ofLog() << "loaded Script " + script;
 	}
 	else {
 		ofLog() << "Lua is not initialized";
@@ -20,7 +24,7 @@ void ofxLuaaa::fnSetup(lua_State* ls) {
 	if (ls == NULL) { return; }
 	lua_getglobal(ls, "setup");
 	if (lua_pcall(ls, 0, 0, 0) != 0) {
-		ofLog() << "error running update function";
+		ofLog() << "error running setup function";
 	}
 }
 
@@ -43,7 +47,6 @@ void ofxLuaaa::fnDraw(lua_State* ls) {
 void ofxLuaaa::fnExit(lua_State* ls) {
 	if (ls == NULL) {return;}
 	lua_getglobal(ls, "exit");
-	//lua_gc(ls, LUA_GCCOLLECT, 0);
 	if (lua_pcall(ls, 0, 0, 0) != 0) {
 		ofLog() << "error running exit function";
 	}
@@ -51,6 +54,26 @@ void ofxLuaaa::fnExit(lua_State* ls) {
 
 void ofxLuaaa::clearLua(lua_State* ls) {
 	if (ls != NULL) {
+		lua_settop(ls, 0);
 		lua_gc(ls, LUA_GCCOLLECT, 0);
 	}
 }
+
+void ofxLuaaa::setBool(lua_State* ls, string var, bool val) {
+	if (ls != NULL) {
+		lua_pushglobaltable(ls);
+		lua_pushboolean(ls, val);
+		lua_setfield(ls, -2, var.c_str());
+		lua_pop(ls, 1);
+	}
+}
+
+void ofxLuaaa::setNumber(lua_State* ls, string var, float val) {
+	if (ls != NULL) {
+		lua_pushglobaltable(ls);
+		lua_pushnumber(ls, val);
+		lua_setfield(ls, -2, var.c_str());
+		lua_pop(ls, 1);
+	}
+}
+
--- src/ofxLuaaa.h
+++ src/ofxLuaaa.h
@@ -10,10 +10,13 @@ public:
 	ofxLuaaa();
 	virtual ~ofxLuaaa();
 
-	void runScript(lua_State* ls, const std::string& script);
+	void runScript(lua_State* ls, std::string& script);
 	void fnSetup(lua_State* ls);
 	void fnUpdate(lua_State* ls);
 	void fnDraw(lua_State* ls);
 	void fnExit(lua_State* ls);
 	void clearLua(lua_State* ls);
+
+	void setBool(lua_State* ls, string var, bool val);
+	void setNumber(lua_State* ls, string var, float val);
 };
